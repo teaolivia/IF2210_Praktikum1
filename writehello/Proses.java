f61fd55c2a15fa118db29c8ebb2057e9bc8f856d
Thea Olivia <theaolivia.mail@gmail.com>
2015-06-15 03:19:57 +0700

    Third commit
    writehello java

---
+++ writehello/Proses.java
@@ -0,0 +1,12 @@
+// File : Proses.java
+
+public class Proses(){
+
+	public Proses(){
+		System.out.println("WriteHello");
+	}
+	public static void main (String[] args){
+	// main
+	Proses object = new Proses(P1, P2, P3);
+}
+}
---
+++ writehello/WriteHello.java
@@ -0,0 +1,9 @@
+// File : WriteHello.java
+
+public class WriteHello(){
+
+	public static void main (String[] args){
+	// main
+	WriteHello object = new WriteHello(P1, P2, P3);
+}
+}
